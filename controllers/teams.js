573f31efdeeb246409a3a8cae2374a3d7f4e6ba7
Thomas Attermann <thomas_5009@hotmail.com>
2017-03-02 16:09:32 +0100

    Added schedule to a specific team .

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,16 +14,19 @@ module.exports.findTeam = function (teamId, callback) {
 
     fs.readFile('./resources/data.json', 'utf8', function (err, data) {
         if (err) throw err; // we'll not consider error handling for now
-        
-        var teams = JSON.parse(data).teams;
-        var users = JSON.parse(data).users;
+
+        var teams       = JSON.parse(data).teams;
+        var users       = JSON.parse(data).users;
+        var schedules   = JSON.parse(data).schedules;
 
         if (teams[teamId-1] != null && users[teamId-1] != null) {
 
             var userWithInTeam = users[teamId-1]
             var teamWithId = teams[teamId-1]
+            var scheduleForTeam = schedules[teamId-1]
 
-            teamWithId.users = userWithInTeam
+            teamWithId.users    = userWithInTeam
+            teamWithId.schedule =  scheduleForTeam
 
             callback(null, teamWithId)
 
