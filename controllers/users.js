8b86cb1b017a117bc45aeaab404fa27c808fa5c5
Steffen <Steffen@MacBook-Pro.local>
2017-03-02 16:00:11 +0100

    Show related user data(team and schedule) for a given user with an id

--- controllers/users.js
+++ controllers/users.js
@@ -16,10 +16,15 @@ module.exports.findUser = function (userId, callback) {
         if (err) throw err; // we'll not consider error handling for now
         var users = JSON.parse(data).users;
         if (users[userId-1] != null) {
-            callback(null, users[userId-1])
+            var user = users[userId-1];
+            var userTeam = JSON.parse(data).teams[user.teamId-1];
+            var userTeamScheduleId = userTeam.scheduleId;
+            var userSchedule = JSON.parse(data).schedules[userTeamScheduleId-1];
+            user.teams = userTeam;
+            user.schedule = userSchedule;
+            callback(null, user)
         } else {
             callback("User not found", null)
         }
     });
-    // Instead of this, run through json document and return user with specific i
 }
