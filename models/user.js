d2e3300a6e7da2b8b137c95f7534c97dece0bddb
Thomas Attermann <thomas_5009@hotmail.com>
2017-04-24 13:53:43 +0200

    Adds mongoose to the project and make User model

---
+++ models/user.js
@@ -0,0 +1,52 @@
+/**
+ * Dependencies
+ */
+const mongoose = require('mongoose');
+const Schema        = mongoose.Schema;
+
+/**
+ * Schema
+ */
+
+const UserSchema = new Schema({
+    password: {
+        type : String,
+        trim : true,
+        select: false
+    },
+    email: {
+        type : String,
+        trim : true,
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    address: {
+        type: String,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        trim: true,
+    },
+    teamId: {
+        type: Number,
+        trim: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    createdAt  : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+mongoose.model('User', UserSchema);
