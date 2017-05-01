7fcc25ebcdbd3e4e30e02ff2b012d1fcab603961
Thomas Attermann <thomas_5009@hotmail.com>
2017-05-01 15:30:29 +0200

    Removed unused functions

--- controllers/users.js
+++ controllers/users.js
@@ -5,35 +5,6 @@ const saltRounds = 10;
 const User = mongoose.model('User');
 var jwt = require('jsonwebtoken');
 
-
-module.exports.findUsers = function (callback) {
-    fs.readFile('./resources/data.json', 'utf8', function (err, data) {
-        if (err) throw err; // we'll not consider error handling for now
-        var users = JSON.parse(data).users;
-        callback(null, users)
-    });
-}
-
-// Find one specific user by id.
-module.exports.findUser = function (userId, callback) {
-
-    fs.readFile('./resources/data.json', 'utf8', function (err, data) {
-        if (err) throw err; // we'll not consider error handling for now
-        var users = JSON.parse(data).users;
-        if (users[userId-1] != null) {
-            var user = users[userId-1];
-            var userTeam = JSON.parse(data).teams[user.teamId-1];
-            var userTeamScheduleId = userTeam.scheduleId;
-            var userSchedule = JSON.parse(data).schedules[userTeamScheduleId-1];
-            user.teams = userTeam;
-            user.schedule = userSchedule;
-            callback(null, user)
-        } else {
-            callback("User not found", null)
-        }
-    });
-}
-
 /**
  * Create a user
  */
