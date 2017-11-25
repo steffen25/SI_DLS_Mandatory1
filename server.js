63f7ac839463c330e24a303b59933ecdd1ed9fa6
Thomas Attermann <thomas_5009@hotmail.com>
2017-11-25 23:17:15 +0100

    Port fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./app');
-const port  = process.env.NODE_PORT || 4000;
+const port  = process.env.PORT || 4000;
 
 app.listen(port, function() {
   console.log("API is running on port " + port);
